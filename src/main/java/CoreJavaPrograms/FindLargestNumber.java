a9484f9ea495e96de501e78297e3479c87dec0a8
MuskanSoftTest <muskanambardekar07@gmail.com>
2025-07-13 19:47:43 +0530

    find_largest_number

--- src/main/java/CoreJavaPrograms/FindLargestNumber.java
+++ src/main/java/CoreJavaPrograms/FindLargestNumber.java
@@ -4,6 +4,16 @@ public class FindLargestNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] arr = {1,5,2,7,9};
+		int max = arr[0];
+		
+		for(int i=1; i<arr.length; i++) {
+			if(arr[i]>max) 
+				max= arr[i];
+			}
+			
+		System.out.println("largest number is " + max);
+		
 
 	}
 
