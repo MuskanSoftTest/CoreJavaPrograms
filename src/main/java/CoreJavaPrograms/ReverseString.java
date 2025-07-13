34df5fadfb8dd9d5fee83eedcaa53114a565fc76
MuskanSoftTest <muskanambardekar07@gmail.com>
2025-07-13 19:01:46 +0530

    initial comment

---
+++ src/main/java/CoreJavaPrograms/ReverseString.java
@@ -0,0 +1,19 @@
+package CoreJavaPrograms;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "Muskan";
+		String reversed = "";
+		
+		for(int i = str.length() -1; i>=0; i--) {
+			
+			reversed = reversed + str.charAt(i);								
+		}
+		System.out.println("Actual String : " + str + "\nReversed String : " +reversed);
+
+	}
+
+}
