016993af1fd3d4ee3b0125347db2cf04f37270e0
MuskanSoftTest <muskanambardekar07@gmail.com>
2025-07-13 19:25:18 +0530

    factorial number

--- src/main/java/CoreJavaPrograms/Palindrom.java
+++ src/main/java/CoreJavaPrograms/Palindrom.java
@@ -2,6 +2,7 @@ package CoreJavaPrograms;
 
 public class Palindrom {
 
+//	A palindrome is a string that reads the same forwards and backwards, e.g. madam, 121, racecar.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -9,8 +10,7 @@ public class Palindrom {
 		String reversed = "";
 		
 		for(int i= str.length() - 1; i>=0; i--) {
-			reversed= reversed +  str.charAt(i);
-			
+			reversed= reversed +  str.charAt(i);		
 		}
 		if(str.equals(reversed)) {
 			System.out.println(str + ": word is palindrom");
